337e0be4802a264bb8ccda1ab682ad73652f6be4
Arun Jacob <arun.jacob@kointrack.com>
2022-06-02 13:34:07 +0530

    initialized project

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+            <div className="yoy">
+                <p>sijdf</p>
+                <span>sdjf</span>
+            </div>
+        </>
+    );
+};
+
+export default Header;
