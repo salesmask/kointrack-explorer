ec77a65a96dcb186c080afacc52ae28a8fc76053
krishnavish99 <krishna.v@kointrack.com>
2022-06-06 11:05:57 +0530

    project initialized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HomeRoute from './routes/home/HomeRoute';
+import './styles/global.css';
+
+function App() {
+    return (
+        <>
+            <HomeRoute />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/homepage/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return <div className="tab__button">Metaverse</div>;
+};
+
+export default Home;
