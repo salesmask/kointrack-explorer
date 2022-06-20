9ee0c10332666f061b12c0156fdeae41035a2daf
Arun Jacob <arun.jacob@kointrack.com>
2022-06-20 13:12:04 +0530

    added deploying in github pages and added breadcrumbs in coindetails

--- src/components/coindetails/CoinDetails.js
+++ src/components/coindetails/CoinDetails.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import PriceDetails from './pricedetails/PriceDetails';
+import Breadcrumbs from './breadcrumbs/Breadcrumbs';
 
 const CoinDetails = () => {
     return (
         <>
             <Header />
+            <Breadcrumbs />
             <PriceDetails />
 
             <Footer />
