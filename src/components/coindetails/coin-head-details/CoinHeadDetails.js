a3a236cb5d6c9f5196286fb7dd5a7601b793df26
Arun Jacob <arun.jacob@kointrack.com>
2022-06-29 23:51:32 +0530

    changed coin-head-details in coin details page with bootstrap model and with responsiveness

--- src/components/coindetails/coin-head-details/CoinHeadDetails.js
+++ src/components/coindetails/coin-head-details/CoinHeadDetails.js
@@ -9,95 +9,137 @@ import ETHArrowUp from '../../../assets/coindetails/coin-head-details/Increase_A
 
 const CoinHeadDetails = () => {
     return (
-        <div className="coin-head__wrapper d-flex justify-content-between align-items-center">
-            {/* Name and details section */}
+        <div className=" container-fluid px-0 yoyo-test">
+            <div className="row mx-0">
+                <div className="col-11 mx-auto d-flex px-0">
+                    <div className="coin-head__wrapper w-100 p-0 p-md-4 ">
+                        {/* Name and details section */}
 
-            <div className="coin-head__name--wrapper">
-                <div className="d-flex justify-content-start align-items-center mb-3">
-                    <div className="image__wrapper pe-3">
-                        <img
-                            src={CoinImage}
-                            alt="coin"
-                        />
-                    </div>
-                    <div className="name pe-3">Bitcoin</div>
-                    <div className="symbol pe-3">BTC</div>
-                    <div className="watchlist__wrapper">
-                        <img
-                            src={WatchListStar}
-                            alt="star-icon"
-                        />
-                    </div>
-                </div>
-                <div className="d-flex justify-content-center align-items-center">
-                    <div className="rank">Rank #1</div>
-                    <span>/</span>
-                    <div className="type">Coin</div>
-                    <span>/</span>
-                    <div className="watchlist-count">
-                        On 3,223,084 watchlists
-                    </div>
-                </div>
-            </div>
+                        <div className="row mx-0 d-flex justify-content-between align-items-center">
+                            <div className="col-12 col-md-5 col-lg-4 col-xl-3 px-0">
+                                <div className="coin-head__name--wrapper d-flex justify-content-between align-items-start align-items-sm-center flex-column flex-sm-row flex-md-column align-items-md-start">
+                                    <div className="d-flex justify-content-start align-items-center mb-3">
+                                        <div className="image__wrapper pe-2 pe-sm-3">
+                                            <img
+                                                src={CoinImage}
+                                                alt="coin"
+                                            />
+                                        </div>
+                                        <div className="name pe-2 pe-sm-3">
+                                            Bitcoin
+                                        </div>
+                                        <div className="symbol pe-2 pe-sm-3">
+                                            BTC
+                                        </div>
+                                        <div className="watchlist__wrapper">
+                                            <img
+                                                src={WatchListStar}
+                                                alt="star-icon"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="d-flex justify-content-start align-items-center text-nowrap">
+                                        <div className="rank text-nowrap">
+                                            Rank #1
+                                        </div>
+                                        <span>/</span>
+                                        <div className="type">Coin</div>
+                                        <span>/</span>
+                                        <div className="watchlist-count text-nowrap">
+                                            On 3,223,084 watchlists
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-            {/* Range bar section */}
-            <div className="coin-head__range--wrapper d-flex justify-content-between align-items-center d-none d-xl-flex">
-                <div className="range__inner--wrapper">
-                    <div className="range__lowhigh--values d-flex justify-content-between align-items-center mb-4">
-                        <div className="low__value">
-                            Low:
-                            <span className="ps-2">$26,350.49</span>
+                            <div className="col-12 col-md-6 col-lg-6 col-xl-8 px-0">
+                                <div className="row mx-0 d-flex justify-content-between align-items-center flex-column-reverse flex-xl-row gap-4">
+                                    <div className="col-12 col-xl-5 px-0">
+                                        {/* Range bar section */}
+                                        <div className="coin-head__range--wrapper d-flex justify-content-xl-start align-items-center justify-content-md-end">
+                                            <div className="range__inner--wrapper">
+                                                <div className="range__lowhigh--values d-flex justify-content-between align-items-center mb-4">
+                                                    <div className="low__value">
+                                                        Low:
+                                                        <span className="ps-2">
+                                                            $26,350.49
+                                                        </span>
+                                                    </div>
+                                                    <div className="high__value">
+                                                        High:
+                                                        <span className="ps-2">
+                                                            $68,789.63
+                                                        </span>
+                                                    </div>
+                                                </div>
+
+                                                <div class="progress w-100 rounded-pill">
+                                                    <div
+                                                        class="progress-bar w-50 rounded-pill"
+                                                        role="progressbar"
+                                                        aria-valuenow="25"
+                                                        aria-valuemin="0"
+                                                        aria-valuemax="100"
+                                                    ></div>
+                                                </div>
+                                            </div>
+                                            <div className="range-of__time rounded-pill d-flex justify-content-between align-items-center ms-4 px-3 py-1">
+                                                <div className="pe-1 pe-sm-3 time">
+                                                    24h
+                                                </div>
+                                                <img
+                                                    src={RangeofTimeIcon}
+                                                    alt="down-icon"
+                                                    className="range-of-time__icon"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-12 col-xl-5 px-0 mt-3 mt-sm-0">
+                                        {/* Current price details section */}
+
+                                        <div className="coin-head__price--wrapper gap-4 d-flex justify-content-between justify-content-md-end align-items-center flex-xl-column align-items-xl-end">
+                                            <div className="name-symbol d-none d-xl-block">
+                                                Bitcoin Price (BTC)
+                                            </div>
+                                            <div className="current__price--wrapper d-flex justify-content-end align-items-center">
+                                                <span className="current-price">
+                                                    $29,651.69
+                                                </span>
+                                                <span className="percent-change ms-3 py-0 px-2 py-md-1 px-md-3 rounded-pill text-nowrap">
+                                                    <img
+                                                        src={
+                                                            CurrentPriceArrowDown
+                                                        }
+                                                        alt="change-icon"
+                                                        className="change-icon"
+                                                    />
+                                                    <span className="ps-1">
+                                                        6.45%
+                                                    </span>
+                                                </span>
+                                            </div>
+                                            <div className="corresponding-current-eth d-md-none d-lg-block">
+                                                <span className="eth__price">
+                                                    16.48 ETH
+                                                </span>
+                                                <span className="eth-change ms-3">
+                                                    <img
+                                                        src={ETHArrowUp}
+                                                        alt="eth-change"
+                                                    />
+                                                    <span className="ps-2">
+                                                        8.45%
+                                                    </span>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="high__value">
-                            High:
-                            <span className="ps-2">$68,789.63</span>
-                        </div>
                     </div>
-
-                    <div class="progress w-100 rounded-pill">
-                        <div
-                            class="progress-bar w-50 rounded-pill"
-                            role="progressbar"
-                            aria-valuenow="25"
-                            aria-valuemin="0"
-                            aria-valuemax="100"
-                        ></div>
-                    </div>
-                </div>
-                <div className="range-of__time rounded-pill d-flex justify-content-between align-items-center ms-4 px-3 py-1">
-                    <div className="pe-3 time">24h</div>
-                    <img
-                        src={RangeofTimeIcon}
-                        alt="down-icon"
-                        className="range-of-time__icon"
-                    />
-                </div>
-            </div>
-
-            {/* Current price details section */}
-
-            <div className="coin-head__price--wrapper text-end">
-                <div className="name-symbol">Bitcoin Price (BTC)</div>
-                <div className="current__price--wrapper d-flex justify-content-center align-items-center">
-                    <span className="current-price">$29,651.69</span>
-                    <span className="percent-change ms-3 py-1 px-3 rounded-pill">
-                        <img
-                            src={CurrentPriceArrowDown}
-                            alt="change-icon"
-                            className="change-icon"
-                        />
-                        <span className="ps-1">6.45%</span>
-                    </span>
-                </div>
-                <div className="corresponding-current-eth">
-                    <span className="eth__price">16.48 ETH</span>
-                    <span className="eth-change ms-3">
-                        <img
-                            src={ETHArrowUp}
-                            alt="eth-change"
-                        />
-                        <span className="ps-2">8.45%</span>
-                    </span>
                 </div>
             </div>
         </div>
