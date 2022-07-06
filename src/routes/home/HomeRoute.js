eeae2d7e599abba327cd894dfd7ca2a2ec4e7913
Arun Jacob <arun.jacob@kointrack.com>
2022-07-06 16:19:50 +0530

    created trending page and added banner section

--- src/routes/home/HomeRoute.js
+++ src/routes/home/HomeRoute.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../../components/homepage/Home';
+import Trending from '../../components/trending/Trending';
 
 const HomeRoute = () => {
     return (
@@ -8,6 +9,10 @@ const HomeRoute = () => {
             <Route
                 path="/"
                 element={<Home />}
+            />
+            <Route
+                path="/trending"
+                element={<Trending />}
             />
         </Routes>
     );
