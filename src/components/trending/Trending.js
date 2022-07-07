04476308828e296a0ebc60f38a0709da96c7263b
Arun Jacob <arun.jacob@kointrack.com>
2022-07-07 12:17:05 +0530

    changed trending banner into seperate component

--- src/components/trending/Trending.js
+++ src/components/trending/Trending.js
@@ -1,32 +1,15 @@
 import React from 'react';
-import './Trending.css';
 import Footer from '../footer/Footer';
 import Header from '../header/Header';
 import TrendingCarousel from './trendingcarousel/TrendingCarousel';
 import TableContainer from './tablecontainer/TableContainer';
+import TrendingBanner from './trendingbanner/TrendingBanner';
 
 const Trending = () => {
     return (
         <>
             <Header />
-
-            <div className="banner__outer--wrapper container-fluid my-5 py-4 px-0">
-                <div className="row h-100 mx-0">
-                    <div className="banner__inner--wrapper py-5 px-5 col-11 mx-auto px-0">
-                        <div className="contents__wrapper">
-                            <h1 className="heading mb-4">
-                                What are the trending Cryptocurrencies on
-                                KOINTRACK?
-                            </h1>
-                            <p className="description">
-                                Below is a list of the trending cryptocurrencies
-                                that people are searching for on KOINTRACK.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <TrendingBanner />
             <TrendingCarousel />
             <TableContainer />
             <Footer />
---
+++ src/components/trending/trendingbanner/TrendingBanner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './TrendingBanner.css';
+
+const TrendingBanner = () => {
+    return (
+        <>
+            <div className="banner__outer--wrapper container-fluid my-5 py-4 px-0">
+                <div className="row h-100 mx-0">
+                    <div className="banner__inner--wrapper py-5 px-5 col-11 mx-auto px-0">
+                        <div className="contents__wrapper">
+                            <h1 className="heading mb-4">
+                                What are the trending Cryptocurrencies on
+                                KOINTRACK?
+                            </h1>
+                            <p className="description">
+                                Below is a list of the trending cryptocurrencies
+                                that people are searching for on KOINTRACK.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default TrendingBanner;
