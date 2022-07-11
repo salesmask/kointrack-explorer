e59af0e84dfcabc6046c8d1ab303e3ef3d73566f
Arun Jacob <arun.jacob@kointrack.com>
2022-07-11 17:01:33 +0530

    changed routes into one file format

---
+++ src/routes/Routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../components/homepage/Home';
+import CoinDetails from '../components/coindetails/CoinDetails';
+import Trending from '../components/trending/Trending';
+import GainersLosers from '../components/gainerslosers/GainersLosers';
+// import SignupForm from '../../components/coindetails/Signupform/SignupForm';
+// import LoginForm from '../../components/coindetails/loginform/LoginForm';
+
+const MainRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="/coindetails" element={<CoinDetails />} />
+
+            <Route path="/trending" element={<Trending />} />
+
+            <Route path="/gainers&losers" element={<GainersLosers />} />
+
+            {/* <Route path="/signupform" element={<SignupForm />} />
+            <Route path="/loginform" element={<LoginForm />} /> */}
+
+        </Routes>
+    );
+};
+
+export default MainRoutes;
