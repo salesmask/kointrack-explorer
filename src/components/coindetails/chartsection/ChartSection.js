7276a9702d587bfedfdf201fe8f0e4a91b238180
Arun Jacob <arun.jacob@kointrack.com>
2022-07-12 11:20:10 +0530

    added more styling to chart time tags in coindetails page

--- src/components/coindetails/chartsection/ChartSection.js
+++ src/components/coindetails/chartsection/ChartSection.js
@@ -73,11 +73,45 @@ const ChartSection = () => {
                             <li>YTD</li>
                             <li>All</li>
                             <li className="calendar__icon--wrapper">
-                                <img
-                                    src={CalendarIcon}
-                                    alt="calendar-icon"
+                                <svg
+                                    width="16"
+                                    height="17"
+                                    viewBox="0 0 16 17"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
                                     className="calender__icon"
-                                />
+                                >
+                                    <rect
+                                        x="1"
+                                        y="4"
+                                        width="14"
+                                        height="12"
+                                        rx="2"
+                                        stroke="#1E202B"
+                                        stroke-width="2"
+                                    />
+                                    <rect
+                                        x="4"
+                                        width="2"
+                                        height="5"
+                                        rx="1"
+                                        fill="#1E202B"
+                                    />
+                                    <rect
+                                        y="7"
+                                        width="16"
+                                        height="2"
+                                        rx="1"
+                                        fill="#1E202B"
+                                    />
+                                    <rect
+                                        x="10"
+                                        width="2"
+                                        height="5"
+                                        rx="1"
+                                        fill="#1E202B"
+                                    />
+                                </svg>
                             </li>
 
                             {/* Need to remove this li for vertical line with css ::before */}
