22365884a17261c78fc61b17197d86806524f027
Arun Jacob <arun.jacob@kointrack.com>
2022-07-25 12:34:47 +0530

    add-banner section in homepage completed

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,6 +6,7 @@ import Search from './layouts/search/Search';
 import Footer from '../../components/footer/Footer';
 import CarouselPannel from '../../components/header/CarouselPannel';
 import CTA from './layouts/cta/CTA';
+import AddBanner from './layouts/addbanner/AddBanner';
 
 const Home = () => {
     return (
@@ -19,6 +20,7 @@ const Home = () => {
             <Highlights />
             <Table />
             <CTA />
+            <AddBanner />
             <Footer />
         </div>
     );
